cf574e6830d116e87cb29b3762ab8b25ad2fdd2c
HadenHiles <haden@howtohockey.com>
2022-10-18 13:11:56 -0400

    Setup and configure scraping endpoints for retrieving player id and player gpg vs opponent from dobbersports

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require('express')
 const app = express()
 const port = 3030
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Try an endpoint!')
+});
+
+app.get('/playerByName/', (req, res) => {
+    var firstName = req.query.firstName.toLowerCase();
+    var lastName = req.query.lastName.toLowerCase();
+    var dobberPlayerProfileUrl = "https://frozenpool.dobbersports.com/players/" + firstName + "-" + lastName;
+
+    rp(dobberPlayerProfileUrl).then((html) => {
+        const $ = cheerio.load(html);
+        //success!
+        var playerIdHtml = $('#hidden_player_id');
+        var playerId = playerIdHtml['0'].attribs.value;
+
+        res.json({
+            dobberUrl: dobberPlayerProfileUrl,
+            playerId: playerId
+        });
+    }).catch((err) => {
+        res.json({error: err.message});
+    });
+});
+
+app.get('/playerGoalsPerGameVSopponent', (req, res) => {
+    var playerId = req.query.playerId;
+    var abbrOpponent = req.query.opp.toUpperCase();
+    var dobberVSopponentUrl = "https://frozenpool.dobbersports.com/frozenpool_playeropponent.php?player=" + playerId + "&opponent=" + abbrOpponent + "&games=R%3A10";
+
+    rp(dobberVSopponentUrl).then((html) => {
+        const $ = cheerio.load(html);
+        var gamesPlayed = parseInt($($('table#opponentStats > tbody > tr:first-child > td:nth-child(1)')['0']).text());
+        var goals = parseInt($($('table#opponentStats > tbody > tr:first-child > td:nth-child(2)')['0']).text());
+        var gpg = Math.round(((goals / gamesPlayed)) * 100) / 100;
+
+        res.json({
+            dobberUrl: dobberVSopponentUrl,
+            playerGPGvsOpponent: gpg
+        });
+    }).catch((err) => {
+        res.json({error: err.message});
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
